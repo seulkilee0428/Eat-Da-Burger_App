68a9499f9cf816cedee489e4ee1de732c5292b55
Seulki Lee <seulki.lee.0428@gmail.com>
2020-07-08 23:41:41 -0500

    revering the change

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(process.env.PORT || 8080, function () {
+app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
